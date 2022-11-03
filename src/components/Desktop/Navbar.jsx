e963e7d5ae3302218899cc19045d9d1a0c0eab4c
Abhisek Upadhaya <Abhisek0721@gmail.com>
2022-11-03 14:15:46 +0530

    fixed desktop navbar UI

--- src/components/Desktop/Navbar.jsx
+++ src/components/Desktop/Navbar.jsx
@@ -36,31 +36,28 @@ const Navbar = (props) =>{
         <Link to={'/'}>
           <div id="logo" className='flex'>
               <img src={require('../img/logo.png')} width='50' alt="Our Quotes" />
-              <h1 className='ml-4 mt-3 text-xl font-bold'>Our Quotes</h1>
+              <h1 className='ml-4 mt-3 text-lg font-bold'>Our Quotes</h1>
           </div>
         </Link>
-        <div className='w-[60%] flex'>
-          <div className=' flex justify-evenly mt-3 w-[75%]'>
-            <Link to={'/'} className='text-xl hover:cursor-pointer hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faHome} className="mr-2" />Home</Link>
+        <div className='w-[70%] flex'>
+          <div className=' flex justify-evenly mt-3 w-[100%]'>
+            <Link to={'/'} className='text-lg hover:cursor-pointer mx-2 hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faHome} className="mr-2" />Home</Link>
             <div>
-              <div onClick={()=>showNotif()} className='text-xl hover:cursor-pointer hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faBell} className="mr-2" />Notifications <span>({numOfNotif})</span></div>
+              <div onClick={()=>showNotif()} className='text-lg mx-2 hover:cursor-pointer hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faBell} className="mr-2" />Notifications <span>({numOfNotif})</span></div>
               <div id="notif-box" className="hidden">
                 <Notifications username={props.username} />
               </div>
             </div>
-            <Link to={''} className='text-xl hover:cursor-pointer hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faQuoteLeft} className="mr-2" />Your Quotes</Link>
-            <Link to={'/contact'} className='text-xl hover:cursor-pointer hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faContactCard} className="mr-2" />Contact Us</Link>
-          </div>
-          <div className='flex justify-between w-[25%]'>
+            <Link to={'/contact'} className='text-lg mx-2 hover:cursor-pointer hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faContactCard} className="mr-2" />Contact Us</Link>
             {(!props.username)?(
               <>
-              <Link to="/login"><button className='text-xl hover:cursor-pointer mt-3 hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faSignIn} className="mr-2" />Sign in</button></Link>
-              <Link to="/signup"><button className='text-lg hover:cursor-pointer h-10 mt-2 bg-red-700 text-white px-5 rounded-md hover:bg-red-800 active:hover:bg-red-600 font-semibold'>Sign up</button></Link>
+              <Link to="/login"><button className='text-lg mx-2 hover:cursor-pointer hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faSignIn} className="mr-2" />Sign in</button></Link>
+              <button to="/signup"><button className='text-lg mx-2 hover:cursor-pointer bg-red-700 text-white px-5 py-1 rounded-md hover:bg-red-800 active:hover:bg-red-600 font-semibold'>Sign up</button></button>
               </>
             ):(
               <>
-              <button className='text-xl hover:cursor-pointer hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faUser} className="mr-2" />{props.username}</button>
-              <button onClick={()=>logout()} className='text-lg hover:cursor-pointer h-10 mt-2 bg-red-700 text-white px-5 rounded-md hover:bg-red-800 active:hover:bg-red-600 font-semibold'>Log out</button>
+              <Link className='text-lg mx-2 hover:cursor-pointer hover:text-red-800 active:hover:text-red-600 font-semibold'><FontAwesomeIcon icon={faUser} className="mr-2" />{props.username}</Link>
+              <button onClick={()=>logout()} className='text-lg mx-2 hover:cursor-pointer mb-1 bg-red-700 text-white px-5 py-1 rounded-md hover:bg-red-800 active:hover:bg-red-600 font-semibold'>Log out</button>
               </>
             )}
           </div>
